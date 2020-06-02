1606548f9a1549d37061a484704ffb482e652ee0
f0rk <dummy.github.1945@gmail.com>
2020-06-02 10:49:57 +0900

    Atom Package Initial Commit

---
+++ spec/atom-istanbul-bar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomIstanbulBarView from '../lib/atom-istanbul-bar-view';
+
+describe('AtomIstanbulBarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
